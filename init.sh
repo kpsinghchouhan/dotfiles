25f405470eea586c0e2cd2b7b549355bd3c4908b
kpsinghchouhan <kpsingh.chouhan@gmail.com>
2024-11-25 15:15:16 +0530

    Added starship symlink create command

--- init.sh
+++ init.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Create symlinks for vim/gvim configuration files
-
 ln -nfs ~/bin/dotfiles/vim/vimrc ~/.vimrc
 ln -nfs ~/bin/dotfiles/vim/gvimrc ~/.gvimrc
 ln -nfs ~/bin/dotfiles/vim ~/.vim
 
 # Create sysmlinks for git configuration file
-
 ln -nfs ~/bin/dotfiles/git/gitconfig_linux ~/.gitconfig
+
+# Create symlink for starship terminal prompt
+ln -nfs ~/bin/dotfiles/starship/starship.toml ~/.config/starship.toml
